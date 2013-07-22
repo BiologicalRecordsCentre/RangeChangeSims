5be0628fea317023b0c9e82a37962fb94c41e165
unknown <njbi@WLPC1701.ad.nerc.ac.uk>
2013-07-22 20:37:49 +0100

    Updated plots

--- Explore_results.R
+++ Explore_results.R
@@ -1,94 +1,100 @@
-# Nick Isaac 19-20 June 2013
+# Nick Isaac 7 May 2013
 # Extract and explore the simulation results
-# We anwt to know if fixed versus variabel number of visits has an  
 # this time we'll collate the output
 
-###########
-
-# Previously (130619) I compared the most recent results with 130502, which was a test run
-# Actually I should compare them with the 'production runs' of 130509
-# In that set, I *thought* that the number of visits was fixed, but actually it was variable
-
-###########
-
+# Updated 29 May to include Power & Occupancy results
+# Updated 22 july to add the mixed model with no threshold for data
 
 library(ggplot2)
 library(reshape2)
-rm(list=objects())
-homedir <- 'P:/NEC04273_SpeciesDistribution/Workfiles/Range change sims' # same location as 'Sim functions 13xxxx.r'
-setwd(homedir)
+
+homedir <- getwd()
 
 source('extract_collate_stats.R')
-source('Sim_functions')
+source('Sim_functions.r')
 
+datecode <- format(Sys.Date(), '%y%m%d')
 
-sinkdir <- 'results/resuts 130619 stochastic=F'
+######################
 
+sinkdir <- 'results/results 130621 +MM0'
 
-nrow(output_stoch <- extract_output('results/results 130509')) #1155000
-nrow(output_fixed <- extract_output('results/resuts 130619 stochastic=F')) #273000
+# first load the production set of results
+nrow(output0 <- extract_output('results/results 130509')) #1155000
 
+# now load the new results: these should be on identical data but only using the mixed model with 0 threshold
+nrow(output1 <- extract_output('results/results 130621 +MM0')) #882000
 
-# check the names match
-all(names(output_stoch) == names(output_fixed)) # T
-
-# I belived the 'stoch' set was created with a fixed number of visits per year. 
-# In fact I made a coding error so it was actually variable (i.e. stochastic)
-output_stoch$stochastic <- 1
-
-
-# which methods have been used?
-setdiff(output_stoch$Statistic, output_fixed$Statistic) # Frescalo & MM absent from the 'fixed' set
-
-# restrict the output to just the columns of interest (i.e. the 'Methods')
-nrow(output_fixed <- subset(output_fixed, grepl('_p$',Statistic))) #56000
-
-# and do the same in the variable set
-nrow(output_stoch <- subset(output_stoch, grepl('_p$',Statistic)
-                            & Statistic %in% unique(output_fixed$Statistic)
-                            #& pSVS %in% unique(output_fixed$pSVS)
-                            )) #168000
-
-# how big are they
-with(output_stoch, table(decline, pSVS)) # 2 times bigger
-with(output_fixed, table(decline, pSVS))
-
-# how many reps per setup?
-max(output_stoch$repnum) # 500!
-max(output_fixed$repnum) # only 250!
-
-     
-output <- droplevels(rbind(output_fixed, output_stoch))
-table(output$stochastic) #8000 F, 7000 T (mismatch is becuase later set includes Maes method)
-
+output <- droplevels(rbind(
+            subset(output0, grepl('_p$',Statistic)),
+            subset(output1, grepl('_p$',Statistic) & grepl('MMbin0', Statistic))
+            ))
 output$Method <- gsub(x=output$Statistic, pattern='_p', repl='')
 
-
-
-######## ######## ######## ######## ######## ######## ######## ######## ######## ######## ######## 
-
-
-
+###################################################################################### ERROR RATES
 # calculate the error rates
-ER <- dcast(output, Method + Scenario + pSVS + stochastic + decline ~ ., fun=error_rate, value.var='value')
+ER <- dcast(output, Method + Scenario + pSVS + decline ~ ., fun=error_rate, value.var='value')
 names(ER)[ncol(ER)] <- 'ER' # default is to call it 'NA'
 
 # get the sample sizes, just for a sense-check
-(ERN <- dcast(output, Method + Scenario + pSVS + stochastic + decline ~ ., fun=length, value.var='value'))
+ER$nreps <- as.numeric(acast(output, Method + Scenario + pSVS + decline ~ ., fun=length, value.var='value'))
 
-ER$combo <- with(ER, paste(stochastic,Method))
+###################################################################################### OCCUPANCY
+# Now import the Occupancy results and match them up.
+# the results file is modified into a readable format from the spreadsheet sent to me by Marnix
+occstats <- read.csv('results/results 130509/Occupancy results 130527.csv')
+occstats$Method <- 'Occupancy'
+occstats$ER <- with(occstats, X..sig.trends/nreps)
+
+# merge
+#ER <- rbind(ER, subset(occstats, select=intersect(names(occstats), names(ER))))
+#ER <- rbind(ER, subset(occstats, select=names(occstats) %in% names(ER)))
+ER <- rbind(ER, occstats[,names(occstats) %in% names(ER)])
+
+ER$intensity <- factor(ER$pSVS, levels=c(0.05,0.07,0.1), labels=c('low','med','high'), ordered=T)
+
+dim(ER) # 630 7   
+
+######################################################################## PLOTTING VALIDITY
+
 
 # build the ggplot object
-ggV <- ggplot(data=subset(ER, decline==0), aes(x=pSVS, y=ER, gr=combo))
-ggV <- ggV + geom_line(aes(col=Method, gr=combo, lty=factor(stochastic)), size=1)
+ggV <- ggplot(data=subset(ER, decline==0), aes(x=pSVS, y=ER, gr=Method))
+#ggV <- ggplot(data=subset(ER, decline==0), aes(x=intensity, y=ER))#, gr=Method))
+ggV <- ggV + geom_line(aes(col=Method), size=1)
 ggV <- ggV + scale_y_log10() + geom_hline(yintercept =0.05)
 ggV <- ggV + ylab('Type I error rate') + xlab('recording intensity')
-ggV <- ggV + geom_point(aes(col=Method, shape=factor(stochastic)), size=3)
-ggV <- ggV + facet_wrap(~Scenario, nrow=2)
+#ggV <- ggV + geom_point(aes(col=Method, size=3))
+ggV <- ggV + facet_wrap(~Scenario, nrow=1)
 ggV
 
 
-png(paste0(sinkdir,'/ER ~stoch 130620.png'), wi=1024, hei=500)
-ggV %+% subset(ER, decline==0 & Scenario != 'C1_pShortLEven'
-               & !Method %in% c('nRecords', 'Maes', 'VRhov'))
+# MAKE SOME PLOTS OF VALIDITY
+png(paste0(sinkdir,'/validityA ',datecode,'.png'), wi=500, hei=500)
+ggV %+% subset(ER, decline==0 & grepl('A', Scenario)) # All scenario A
 dev.off()
+
+png(paste0(sinkdir,'/validity1 ',datecode,'.png'), wi=1024, hei=500)
+ggV # All validity 
+dev.off()
+
+png(paste0(sinkdir,'/validity2 ',datecode,'.png'), wi=1024, hei=500)
+ggV %+% subset(ER, decline==0 &  Scenario != 'C1_pShortLEven'
+               & !Method %in% c('nRecords', 'Maes', 'VRhov')) # all except the really bad methods
+dev.off()
+
+png(paste0(sinkdir,'/validityMM ',datecode,'.png'), wi=1024, hei=500)
+ggV %+% subset(ER, decline==0 & grepl('MM', Method))# just MM
+dev.off()
+
+png(paste0(sinkdir,'/validity3 ',datecode,'.png'), wi=1024, hei=500)
+ggV %+% subset(ER, decline==0 & Scenario != 'C1_pShortLEven'
+               & !Method %in% c('nRecords', 'Maes')
+               & !grepl('VR', Method)
+               & !grepl('LL', Method) 
+               & !grepl('A', Scenario)
+               & !grepl('MMber', Method)
+               )# the good methods
+dev.off()
+
+############ NOW REPEAT FOR POWER
