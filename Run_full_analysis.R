8963bc11fd99bc8ecfa6334c3b7101b54a98782f
Tom August <tomaug@ceh.ac.uk>
2013-10-03 14:49:17 +0100

    Script added to install required packages

--- Run_full_analysis.R
+++ Run_full_analysis.R
@@ -2,9 +2,25 @@
 # Tom August
 # 03/10/2013
 
-# sparta package is required
+# WARNING: This script will install libraries needed for the analysis.
+# This includes updateing lme4 if the version you have is <1.0. If you
+# do not want to have these packages installed do not run this script.
+
+# install required packages if not installed
+req_pkgs <- c('lme4','reshape2','abind','sp','gdata','lattice','Matrix')
+inst_pkgs <- req_pkgs[!req_pkgs %in% installed.packages()]
+if(length(inst_pkgs) > 0){
+  install.packages(inst_pkgs)
+}
+
 if(!'sparta' %in% installed.packages()){
   install.packages('sparta_0.1.19.zip', repos = NULL)
+}
+
+# Check that we have a version of lme4 > 1.0
+lme4_details <- installed.packages()['lme4',]
+if(!lme4_details['Version']>1.0){
+  install.packages('lme4')
 }
 
 library(sparta)
